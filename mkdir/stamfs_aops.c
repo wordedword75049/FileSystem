7e6d0351c31a3a54971fe99c114ee1ae6480f4d9
wordedword75049 <naik.iva@gmail.com>
2020-05-28 10:33:34 +0300

    Update stamfs_aops.c

--- mkdir/stamfs_aops.c
+++ mkdir/stamfs_aops.c
@@ -1,12 +1,3 @@
-/* This file is part of stamfs, a GNU/Linux kernel module that */
-/* implements a file-system for accessing STAM devices.        */
-/*                                                             */
-/* Copyright (C) 2004 guy keren, choo@actcom.co.il             */
-/* License: GNU General Public License                         */
-
-/*
- *$Id$
- */
 
 #include "stamfs_aops.h"
 
