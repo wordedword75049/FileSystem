73576fcf4090bdd9c254fe5ca5921127b4f3043b
wordedword75049 <naik.iva@gmail.com>
2020-05-28 10:31:31 +0300

    Update stamfs_super.h

--- justmount/stamfs_super.h
+++ justmount/stamfs_super.h
@@ -1,12 +1,4 @@
-/* This file is part of stamfs, a GNU/Linux kernel module that */
-/* implements a file-system for accessing STAM devices.        */
-/*                                                             */
-/* Copyright (C) 2004 guy keren, choo@actcom.co.il             */
-/* License: GNU General Public License                         */
 
-/*
- *$Id$
- */
 
 #ifndef STAMFS_SUPER_H
 #define STAMFS_SUPER_H
