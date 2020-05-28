ca0ad776beef81b22f296e8b0ba596a816a1a78f
wordedword75049 <naik.iva@gmail.com>
2020-05-28 10:29:01 +0300

    Update stamfs_dir.h

--- filedata/stamfs_dir.h
+++ filedata/stamfs_dir.h
@@ -1,13 +1,3 @@
-/* This file is part of stamfs, a GNU/Linux kernel module that */
-/* implements a file-system for accessing STAM devices.        */
-/*                                                             */
-/* Copyright (C) 2004 guy keren, choo@actcom.co.il             */
-/* License: GNU General Public License                         */
-
-/*
- *$Id$
- */
-
 #ifndef STAMFS_DIR_H
 #define STAMFS_DIR_H
 
